30864eb94ac78a6be94f3e62c4067527aa3a3e7a
Tian Peng <ianpeng97@hotmail.com>
2022-06-03 00:26:05 -0400

    1c completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,22 @@
 import { useState } from 'react'
 
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   const [counter, setCounter] = useState(0) // state hook
 
-  setTimeout(() => setCounter(counter + 1), 1000)
-  return <div>{counter}</div>
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="reset" />
+    </>
+  )
 }
 
 export default App
