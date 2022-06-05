e1f55996eb503d4207e367131fda348b7616c0f9
Tian Peng <ianpeng97@hotmail.com>
2022-06-04 20:51:35 -0400

    add key to Course component in App

--- part1-ex/src/App.jsx
+++ part1-ex/src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
   return (
     <>
       {courses.map((course) => (
-        <Course course={course} />
+        <Course key={course.id} course={course} />
       ))}
     </>
   )
