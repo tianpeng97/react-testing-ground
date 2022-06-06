13efb4584cbf999216ecce79b7471e1531bb18bd
Tian Peng <ianpeng97@hotmail.com>
2022-06-05 23:55:59 -0400

    continue form in App

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+
+  const addNote = (event) => {
+    // prevent page reload
+    event.preventDefault()
+    // target is which component triggered
+    console.log('button clicked', event.target)
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -10,6 +25,10 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
