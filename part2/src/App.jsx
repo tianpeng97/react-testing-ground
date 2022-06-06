5ef90b3c5188b73853592f8ce692d57662929260
Tian Peng <ianpeng97@hotmail.com>
2022-06-06 00:11:05 -0400

    complete forms part

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -4,12 +4,23 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     // prevent page reload
     event.preventDefault()
     // target is which component triggered
     console.log('button clicked', event.target)
+
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
   const handleNoteChange = (event) => {
@@ -17,11 +28,18 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
