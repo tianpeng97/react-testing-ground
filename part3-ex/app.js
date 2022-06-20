95cfb7a5e3a414df715fd3c6bcfc545be09a5d2f
Tian Peng <ianpeng97@hotmail.com>
2022-06-20 16:35:36 -0400

    fix request logger bug

--- part3-ex/app.js
+++ part3-ex/app.js
@@ -20,10 +20,12 @@ mongoose
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(middleware.requestLogger())
 
 app.use('/info', infoRouter)
 app.use('/api/persons', personsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+
+module.exports = app
